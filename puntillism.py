5b722754acfcfe94be2b837b7960542f3ccbcead
James Cameron <quozl@laptop.org>
2019-12-10 16:34:15 +1100

    Restore mirror view if available
    Different versions of PyGame do fail with "SystemError: Bad call flags
    in PyCFunction_Call. METH_OLDARGS is no longer supported!" when
    Camera.set_controls is called, so when that happens ignore it.
    
    As a result, mirror view is disabled silently.

--- puntillism.py
+++ puntillism.py
@@ -65,8 +65,11 @@ class Puntillism():
         clock = pygame.time.Clock()
 
         cam = camera.Camera("/dev/video0", (640, 480), "RGB")
-        #cam.set_controls(1, 0)
         cam.start()
+        try:
+            cam.set_controls(hflip=True)
+        except SystemError:
+            pass
 
         cap = pygame.surface.Surface((640, 480), 0, screen)
         frames = 0
